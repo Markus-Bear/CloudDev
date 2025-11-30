046cc2502556e57a4adbaa885d862a0158252fd1
Markus-Bear <162299946+Markus-Bear@users.noreply.github.com>
2025-11-30 15:28:50 +0000

    lets see what switching on debugging gives us

--- flask/xword/app.py
+++ flask/xword/app.py
@@ -5,8 +5,17 @@ app = Flask(__name__)
 
 @app.get("/hello")
 def hello():
-    return "Hello from the smallest Flask webapp there is!"
+    return "Hello from the really smallest Flask webapp there is!"
 
+
+@app.get("/bye")
+def byebye():
+    return "Goodbye from the really smallest Flask webapp there is!"
+
+
+@app.get("/whoops")
+def omg():
+    return 1 / 0 # This will raise a ZeroDivisionError
 
 if __name__== "__main__":
-    app.run()
+    app.run(debug=True)
