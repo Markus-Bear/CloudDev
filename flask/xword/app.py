94375a2aa18874c197a9b577c8b9f2d59ce96771
Markus-Bear <162299946+Markus-Bear@users.noreply.github.com>
2025-12-01 17:50:47 +0000

    again

--- flask/xword/app.py
+++ flask/xword/app.py
@@ -1,32 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
+import model
+import xword
 
 app = Flask(__name__)
 
-
-@app.get("/hello")
-def hello():
+@app.get("/")
+def opening_page():
     return render_template(
-        "hello.html",
-        the_title="This is HELLO",
+        "pattern.html",
+        the_title="Welcome to Xword on the Web",
     )
 
 
-@app.get("/bye")
-def byebye():
-    return "Goodbye from the really smallest Flask webapp there is!"
 
+@app.post("/processpattern")
+def get_the_results():
+    pat = request.form["pattern"]
+    results = xword.find_possible_matches(pat)
 
-@app.get("/whoops")
-def omg():
-    first = 10
-    second = 50
+    model.add_to_database(pat, len(results)) 
 
     return render_template(
-        "whoops.html",
-        result=first / second,
-        the_title="The Results are in!",
+        "results.html",
+        the_title="Possible matches",
+        the_results = results,
     )
 
+@app.get("/displayhistory")
+def show_the_log():
+    data = model.get_log_data()
+    return render_template(
+        "history.html",
+        the_title="Log Entries",
+        data_table=data,
+    )
 
 if __name__ == "__main__":
     app.run(debug=True)
--- flask/xword/model.py
+++ flask/xword/model.py
@@ -1,11 +1,25 @@
 import DBcm # type: ignore
 
-creds = {
-    "user": "xworduser",
-    "password": "xwordpasswd",
-    "host": "localhost",
-    "database": "xwordDB",
-}
+import platform
+
+if "aws" in platform.uname().release:
+
+    creds ={
+        "user": "c00166672",
+        "password": "xwordpasswd",
+        "host": "c00166672.mysql.pythonanywhere-services.com",
+        "database": "c00166672$default",
+    }    
+else:
+    creds = {
+        "user": "xworduser",
+        "password": "xwordpasswd",
+        "host": "localhost",
+        "database": "xwordDB",
+    }
+
+
+
 
 def add_to_database(p, m):
     SQL = """
